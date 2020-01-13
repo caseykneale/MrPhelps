b815c0b2f79b7b1e4dc5dabe06438d8369091c65
caseykneale <caseykneale@localhost.localdomain>
2020-01-13 18:15:15 -0500

    small clean up

--- MrPhelps/messingaround_structs.jl
+++ MrPhelps/messingaround_structs.jl
@@ -1,22 +1,20 @@
 using Pkg
 Pkg.API.develop(Pkg.PackageSpec(name="MrPhelps", path="/home/caseykneale/Desktop/Playground/MrPhelps/"))
-using MrPhelps
-using Distributed, ClusterManagers, SharedArrays
-using Dates
+using MrPhelps, Distributed, ClusterManagers, SharedArrays, Dates
 
 localonly = true
 #                        Connect Machines!
 #First a user needs to define machines it has access too...
 MrPhelps.greet()
-if !localonly
-    run(`scp -r /home/caseykneale/Desktop/Playground optics@192.168.0.14:/home/optics/`)
-end
+if !localonly;  run(`scp -r /home/caseykneale/Desktop/Playground optics@192.168.0.14:/home/optics/`); end;
 #Define NodeManager - to keep track of Distributed Hooks to all machines
 nm = NodeManager()
+Local  = "127.0.0.1"
+Remote = "optics@192.168.0.14"
 #Connect to a remote box and use a core/thread on the local machine!
 LocalNode   = @async addprocs( 2; restrict = true )
 if !localonly
-    RemoteNode  = @async addprocs( [ ( "optics@192.168.0.14", 2 ) ], tunnel = true, max_parallel = 4,
+    RemoteNode  = @async addprocs( [ ( Remote, 2 ) ], tunnel = true, max_parallel = 4,
                                     exename = "/home/optics/julia-1.3.0/bin/julia") #, sshflags = "-vvv" )
 end
 
@@ -24,37 +22,23 @@ end
     using Pkg
     pth = "/home/caseykneale/Desktop/Playground/MrPhelps/"
     pth = isdir( "/home/caseykneale/" ) ? pth : "/home/optics/Playground/MrPhelps/"
-    if !isdir( "/home/caseykneale/" )
-        #run(`cd /home/optics/Playground/`)
-        #run(`git pull`)
-        #run(`cd`)
-    end
     Pkg.API.develop(Pkg.PackageSpec(name="MrPhelps", path=pth))
 end
 @everywhere using MrPhelps
-
-#Update our node manager, we've added connections
+#Update our node manager, because we've added connections
+#Or we could make the node manager after adding the connections
 update!(nm)
-
 #Grok the machines we have available to our node manager...
-nm.machinenodemap
+println( nm.machinenodemap )
 
 #                        Define Some Tasks
-function clean_data(src::String)
-    println("I read the file \n pscyhe I do nothing!")
-end
-
-Local  = "127.0.0.1"
-Remote = "optics@192.168.0.14"
-
 mission = MissionGraph()
 #Add an unconnected node to the graph
 add_node!(mission, Stash("/home/caseykneale/Desktop/megacsv.csv", [ Remote ] ) )
 #Add a new node to the graph but connect it to the last node laid down
 attach_node!(mission, Agent( clean_data, [ Remote ] ) )
+#Add another new node, but give it a bookmark so we can find it later!
 attach_node!(mission, :Prod1 => Agent( prod, [ Remote ] ) )
-
-println( keys( nm.machinenodemap ) )
 
 #basically repeat the same chain but this is isolated...
 # add_node!(mission, Stash("/home/caseykneale/Desktop/megacsv2.csv", [ Remote ] ) )
@@ -88,8 +72,3 @@ nm.machinenodemap
 #Need a handler to emit when a task is done
 #Need an error handler...
 #Need a way to coelesce data
-
-
-
-
-mission
